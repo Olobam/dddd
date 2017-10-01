bfdbcfcea64f94fae2317bea52b23b85c928adef
DamnedN00b <singed1306@gmail.com>
2017-10-01 16:52:39 +0200

    Added MiniMap tracker

--- LazyUtility.lua
+++ LazyUtility.lua
@@ -12,29 +12,34 @@ function LazyUtility:OnLoad()
 	}
 
 	self:LoadMenu()
+	self:CreateHeroTable()
+
 	Callback.Add("Draw", function() self:OnDraw() end)
 end
 
 function LazyUtility:LoadMenu()
 
-	self.trackerMenu = MenuElement({id = "wPtrackerMenu", name = "Lazy Utility", type = MENU, leftIcon = self.Nudes.Sexy })
-		self.trackerMenu:MenuElement({ id = "wPtracker", name = "WayPoint Tracker", type = MENU, leftIcon = self.Nudes.Buttplug })
-			self.trackerMenu.wPtracker:MenuElement({ id = "toggle", name = "On / Off Toggle", key = string.byte("T"), toggle = true })
-			self.trackerMenu.wPtracker:MenuElement({ id = "eta", name = "Show ETA", value = true })
+	self.Menu = MenuElement({id = "MainMenu", name = "Lazy Utility", type = MENU, leftIcon = self.Nudes.Sexy })
+		self.Menu:MenuElement({ id = "wPtracker", name = "WayPoint Tracker", type = MENU, leftIcon = self.Nudes.Buttplug })
+			self.Menu.wPtracker:MenuElement({ id = "toggle", name = "On / Off Toggle", key = string.byte("T"), toggle = true })
+			self.Menu.wPtracker:MenuElement({ id = "eta", name = "Show ETA", value = true })
 
-			self.trackerMenu.wPtracker:MenuElement({ id = "showAllies", name = "Show Ally Paths", value = true })
-				self.trackerMenu.wPtracker:MenuElement({ id = "allyColors", name = "Ally Color", tooltip = "color and opacity for your teams paths", type = MENU, leftIcon = self.Nudes.Rainbows })
-					self.trackerMenu.wPtracker.allyColors:MenuElement({ id = "opacity", name = "Opacity", value = 255, min = 0, max = 255, step = 1 })
-					self.trackerMenu.wPtracker.allyColors:MenuElement({ id = "red", name = "Red", value = 0, min = 0, max = 255, step = 1 })
-					self.trackerMenu.wPtracker.allyColors:MenuElement({ id = "green", name = "Green", value = 255, min = 0, max = 255, step = 1 })
-					self.trackerMenu.wPtracker.allyColors:MenuElement({ id = "blue", name = "Blue", value = 0, min = 0, max = 255, step = 1 })
+			self.Menu.wPtracker:MenuElement({ id = "showAllies", name = "Show Ally Paths", value = true })
+				self.Menu.wPtracker:MenuElement({ id = "allyColors", name = "Ally Color", tooltip = "color and opacity for your teams paths", type = MENU, leftIcon = self.Nudes.Rainbows })
+					self.Menu.wPtracker.allyColors:MenuElement({ id = "opacity", name = "Opacity", value = 255, min = 0, max = 255, step = 1 })
+					self.Menu.wPtracker.allyColors:MenuElement({ id = "red", name = "Red", value = 0, min = 0, max = 255, step = 1 })
+					self.Menu.wPtracker.allyColors:MenuElement({ id = "green", name = "Green", value = 255, min = 0, max = 255, step = 1 })
+					self.Menu.wPtracker.allyColors:MenuElement({ id = "blue", name = "Blue", value = 0, min = 0, max = 255, step = 1 })
 
-			self.trackerMenu.wPtracker:MenuElement({ id = "showEnemies", name = "Show Enemy Paths", value = true })
-				self.trackerMenu.wPtracker:MenuElement({ id = "enemyColors", name = "Enemy Color", tooltip = "color and opacity for enemy teams paths", type = MENU, leftIcon = self.Nudes.Rainbows })
-					self.trackerMenu.wPtracker.enemyColors:MenuElement({ id = "opacity", name = "Opacity", value = 255, min = 0, max = 255, step = 1 })
-					self.trackerMenu.wPtracker.enemyColors:MenuElement({ id = "red", name = "Red", value = 255, min = 0, max = 255, step = 1 })
-					self.trackerMenu.wPtracker.enemyColors:MenuElement({ id = "green", name = "Green", value = 0, min = 0, max = 255, step = 1 })
-					self.trackerMenu.wPtracker.enemyColors:MenuElement({ id = "blue", name = "Blue", value = 0, min = 0, max = 255, step = 1 })
+			self.Menu.wPtracker:MenuElement({ id = "showEnemies", name = "Show Enemy Paths", value = true })
+				self.Menu.wPtracker:MenuElement({ id = "enemyColors", name = "Enemy Color", tooltip = "color and opacity for enemy teams paths", type = MENU, leftIcon = self.Nudes.Rainbows })
+					self.Menu.wPtracker.enemyColors:MenuElement({ id = "opacity", name = "Opacity", value = 255, min = 0, max = 255, step = 1 })
+					self.Menu.wPtracker.enemyColors:MenuElement({ id = "red", name = "Red", value = 255, min = 0, max = 255, step = 1 })
+					self.Menu.wPtracker.enemyColors:MenuElement({ id = "green", name = "Green", value = 0, min = 0, max = 255, step = 1 })
+					self.Menu.wPtracker.enemyColors:MenuElement({ id = "blue", name = "Blue", value = 0, min = 0, max = 255, step = 1 })
+
+		self.Menu:MenuElement({ id = "mMtracker", name = "Minimap Tracker", type = MENU })
+			self.Menu.mMtracker:MenuElement({ id = "show", name = "MiniMap tracker", value = true, tooltip = "tracks last seen position on MiniMap" })
 end
 
 function LazyUtility:GetDistanceSqr(p1, p2)
@@ -93,23 +98,23 @@ function LazyUtility:DrawWayPoints()
 		for i = 1, #wayPoints do
 
 			if currentHero.pathing.hasMovePath then
-				if currentHero.isAlly and self.trackerMenu.wPtracker.showAllies:Value() then
-					if self.trackerMenu.wPtracker.eta:Value() then
+				if currentHero.isAlly and self.Menu.wPtracker.showAllies:Value() then
+					if self.Menu.wPtracker.eta:Value() then
 						Draw.Text(ETA, 10, wayPoints[#wayPoints]:To2D().x + 10, wayPoints[#wayPoints]:To2D().y + 10,
-							Draw.Color(self.trackerMenu.wPtracker.allyColors.opacity:Value(), self.trackerMenu.wPtracker.allyColors.red:Value(), self.trackerMenu.wPtracker.allyColors.green:Value(), self.trackerMenu.wPtracker.allyColors.blue:Value())) -- Maybe fix those 200 lines long names by using a local? *cough
+							Draw.Color(self.Menu.wPtracker.allyColors.opacity:Value(), self.Menu.wPtracker.allyColors.red:Value(), self.Menu.wPtracker.allyColors.green:Value(), self.Menu.wPtracker.allyColors.blue:Value())) -- Maybe fix those 200 lines long names by using a local? *cough
+					end				
+					if i <= #wayPoints - 1 then
+						Draw.Line(wayPoints[i]:To2D().x, wayPoints[i]:To2D().y, wayPoints[i + 1]:To2D().x, wayPoints[i + 1]:To2D().y, 1, 
+							Draw.Color(self.Menu.wPtracker.allyColors.opacity:Value(), self.Menu.wPtracker.allyColors.red:Value(), self.Menu.wPtracker.allyColors.green:Value(), self.Menu.wPtracker.allyColors.blue:Value()))
+					end
+				elseif currentHero.isEnemy and self.Menu.wPtracker.showEnemies:Value() then
+					if self.Menu.wPtracker.eta:Value() then
+						Draw.Text(ETA, 10, wayPoints[#wayPoints]:To2D().x + 10, wayPoints[#wayPoints]:To2D().y + 10,
+							Draw.Color(self.Menu.wPtracker.enemyColors.opacity:Value(), self.Menu.wPtracker.enemyColors.red:Value(), self.Menu.wPtracker.enemyColors.green:Value(), self.Menu.wPtracker.enemyColors.blue:Value()))
 					end
 					if i <= #wayPoints - 1 then
 						Draw.Line(wayPoints[i]:To2D().x, wayPoints[i]:To2D().y, wayPoints[i + 1]:To2D().x, wayPoints[i + 1]:To2D().y, 1, 
-							Draw.Color(self.trackerMenu.wPtracker.allyColors.opacity:Value(), self.trackerMenu.wPtracker.allyColors.red:Value(), self.trackerMenu.wPtracker.allyColors.green:Value(), self.trackerMenu.wPtracker.allyColors.blue:Value()))
-					end
-				elseif currentHero.isEnemy and self.trackerMenu.wPtracker.showEnemies:Value() then
-				if self.trackerMenu.wPtracker.eta:Value() then
-						Draw.Text(ETA, 10, wayPoints[#wayPoints]:To2D().x + 10, wayPoints[#wayPoints]:To2D().y + 10,
-							Draw.Color(self.trackerMenu.wPtracker.enemyColors.opacity:Value(), self.trackerMenu.wPtracker.enemyColors.red:Value(), self.trackerMenu.wPtracker.enemyColors.green:Value(), self.trackerMenu.wPtracker.enemyColors.blue:Value()))
-					end
-					if i <= #wayPoints - 1 then
-						Draw.Line(wayPoints[i]:To2D().x, wayPoints[i]:To2D().y, wayPoints[i + 1]:To2D().x, wayPoints[i + 1]:To2D().y, 1, 
-							Draw.Color(self.trackerMenu.wPtracker.enemyColors.opacity:Value(), self.trackerMenu.wPtracker.enemyColors.red:Value(), self.trackerMenu.wPtracker.enemyColors.green:Value(), self.trackerMenu.wPtracker.enemyColors.blue:Value()))
+							Draw.Color(self.Menu.wPtracker.enemyColors.opacity:Value(), self.Menu.wPtracker.enemyColors.red:Value(), self.Menu.wPtracker.enemyColors.green:Value(), self.Menu.wPtracker.enemyColors.blue:Value()))
 					end 
 				end	
 			end
@@ -117,8 +122,47 @@ function LazyUtility:DrawWayPoints()
 	end
 end
 
+local heroTable = {}
+
+function LazyUtility:CreateHeroTable()
+
+	for h = 1, Game.HeroCount() do
+
+		local currentHero = Game.Hero(h)
+
+		if currentHero.isEnemy then
+			table.insert( heroTable, { hero = currentHero, lastSeen = GetTickCount() } )
+		end
+	end
+end
+
+function LazyUtility:TrackHeroes()
+
+	for i, v in pairs(heroTable) do
+		if v.hero.visible then
+			heroTable[i].lastSeen = GetTickCount()			
+		elseif not v.hero.visible and not v.hero.dead then
+
+			Draw.CircleMinimap(v.hero.pos, 900, 2, Draw.Color(255, 255, 0, 0)) -- Circle on MM
+
+			local shortTime = tonumber(string.format("%.1f", (GetTickCount()*0.001 - v.lastSeen*0.001)))
+			local shortName = string.sub(v.hero.charName, 0, 4)
+
+			Draw.Text(shortName.. ": " ..shortTime, 9, v.hero.pos:ToMM().x - 20, v.hero.pos:ToMM().y, Draw.Color(255, 255, 255, 255)) -- Text on MM
+
+			Draw.Text(shortName.. ": " ..shortTime, 9, v.hero.pos:To2D(), Draw.Color(255, 255, 255, 255)) -- draw on ground aswell
+		end
+	end
+
+end
+
 function LazyUtility:OnDraw()
 
-	if not self.trackerMenu.wPtracker.toggle:Value() then return end
-	self:DrawWayPoints()
+	if self.Menu.wPtracker.toggle:Value() then
+		self:DrawWayPoints()
+	end
+	if self.Menu.mMtracker.show:Value() then
+		self:TrackHeroes()
+	end
+
 end
